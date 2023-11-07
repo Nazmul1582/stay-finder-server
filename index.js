a8c9a72f16ce3cccce1fedc7f73f8196e82c44df
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-07 07:56:14 +0600

    made connection with mongodb and create token for login

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 
 app.get("/", (req, res) => {
     res.send("Stay Finder is running")
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vatgn7i.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const roomCollection = client.db("roomDB").collection("rooms")
+
+    // auth related api
+    app.post('/login', async(req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1h"})
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "none"
+        })
+        .send({success: true})
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
     console.log(`Stay Finder server is running on PORT: ${port}`);
