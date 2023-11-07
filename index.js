aa8ae8756695d6951bbc86ead188217a0538db6e
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-07 18:33:04 +0600

    created a get api for loaded a specific data

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ async function run() {
         res.send(result)
     })
 
+    app.get("/room-details/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await roomCollection.findOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
